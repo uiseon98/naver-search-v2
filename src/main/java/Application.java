42c0f56a5c2105fbc8ac18c0f047dd7ee77795da
qus0in <qus0in@gmail.com>
2025-03-06 10:27:42 +0900

    Application 구동 흐름

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,6 @@
 import io.github.cdimascio.dotenv.Dotenv;
+import service.searchapi.NaverSearchAPI;
+import service.searchapi.SearchAPI;
 
 import java.util.logging.Logger;
 
@@ -6,6 +8,9 @@ public class Application {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
         Logger logger = Logger.getLogger(Application.class.getName());
-        logger.info(dotenv.get("SEARCH_KEYWORD"));
+        String searchKeyword = dotenv.get("SEARCH_KEYWORD");
+        logger.info(searchKeyword);
+        SearchAPI searchAPI = new NaverSearchAPI();
+        searchAPI.searchByKeyword(searchKeyword);
     }
 }
