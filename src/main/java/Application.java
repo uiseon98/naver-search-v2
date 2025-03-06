523fcc624e35ffa0382306a051b80ea229e9afaa
qus0in <qus0in@gmail.com>
2025-03-06 12:59:36 +0900

    파일까지

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,15 @@
 import io.github.cdimascio.dotenv.Dotenv;
 import model.dto.NaverAPIResult;
 import model.dto.NaverAPIResultItem;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import service.searchapi.NaverSearchAPI;
 import util.logger.MyLogger;
 
+import java.io.FileOutputStream;
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class Application {
@@ -13,9 +19,25 @@ public class Application {
         String searchKeyword = dotenv.get("SEARCH_KEYWORD");
         logger.info(searchKeyword);
         NaverSearchAPI searchAPI = new NaverSearchAPI();
-        try {
+        try (Workbook workbook = new XSSFWorkbook(); FileOutputStream fileOut = new FileOutputStream("%d_%s.xlsx".formatted(System.currentTimeMillis(), searchKeyword))) {
             List<NaverAPIResultItem> result = searchAPI.searchByKeyword(searchKeyword);
-            logger.info(result.toString());
+//            logger.info(result.toString());
+            Sheet sheet = workbook.createSheet(searchKeyword);
+            Row headerRow = sheet.createRow(0);
+            headerRow.createCell(0).setCellValue("날짜");
+            headerRow.createCell(1).setCellValue("링크");
+            headerRow.createCell(2).setCellValue("제목");
+            headerRow.createCell(3).setCellValue("설명");
+            int i = 0;
+            for (NaverAPIResultItem item : result) {
+                Row row = sheet.createRow(++i);
+                row.createCell(0).setCellValue(item.pubDate());
+                row.createCell(1).setCellValue(item.link());
+                row.createCell(2).setCellValue(item.title());
+                row.createCell(3).setCellValue(item.description());
+            }
+           workbook.write(fileOut);
+
         } catch (Exception e) {
           logger.severe(e.getMessage());
         }
